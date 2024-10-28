3c92f8eedc4c8ad1cf0d5767cddcc09d2eb21006
Humberto Guedes <franciscohguedes@hotmail.com>
2024-10-28 03:13:39 -0300

    Build(API_CAEFMD): injeção de dependência da camada de application (MediaR, Automapper)

---
+++ API/CAEFMR/src/Core/CAEFMR.Application/ServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CAEFMR.Application;
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddApplicationLayer(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+        return services;
+    }
+}
