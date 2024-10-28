d2b8507cb9097ae0cbc087373b8d1db048b4d2a6
Humberto Guedes <franciscohguedes@hotmail.com>
2024-10-28 03:15:53 -0300

    Build(API_CAEFMD): injeção de dependência da camada de persistence (EntityFramework)

---
+++ API/CAEFMR/src/Infrastructure/CAEFMR.Persistence/ServiceRegistration.cs
@@ -0,0 +1,23 @@
+﻿using CAEFMR.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAEFMR.Persistence;
+
+public static class ServiceRegistration
+{
+    public static IServiceCollection AddPersistenceLayer(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(options => {
+            options.UseSqlServer(configuration.GetConnectionString("AppDbContext"));
+        });
+
+        return services;
+    }
+}
