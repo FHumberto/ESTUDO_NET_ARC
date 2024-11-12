fc17778ec32691366fa6aab8ae10183aa56ab71d
Humberto Guedes <franciscohguedes@hotmail.com>
2024-11-12 07:51:41 -0300

    [API/CAEFMR]-Misc: atualiza as respostas e insere os anotation de authorize

--- API/CAEFMR/src/Presentation/CAEFMR.Api/Controllers/v1/ExampleController.cs
+++ API/CAEFMR/src/Presentation/CAEFMR.Api/Controllers/v1/ExampleController.cs
@@ -5,16 +5,18 @@ using CAEFMR.Application.Features.Example.Commands.Update;
 using CAEFMR.Application.Features.Example.Queries.GetById;
 using CAEFMR.Application.Features.Example.Queries.GetList;
 using CAEFMR.Domain.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace CAEFMR.Api.Controllers.v1;
 
 [ApiVersion("1")]
-//[Authorize(Roles = "Administrator")]
+[Authorize(Roles = "Administrator")]
 public class ExampleController : BaseApiController
 {
     [HttpGet]
+    [AllowAnonymous]
     [SwaggerOperation(Summary = "Obter todos os exemplos", Description = "Retorna uma lista de todos os exemplos.")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<List<ExampleDto>>> Get()
@@ -25,6 +27,7 @@ public class ExampleController : BaseApiController
 
     [HttpGet("{id:int}")]
     [SwaggerOperation(Summary = "Obter um exemplo pelo ID", Description = "Retorna um exemplo específico com base no ID fornecido.")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ExampleDto>> GetById(int id)
@@ -35,6 +38,7 @@ public class ExampleController : BaseApiController
 
     [HttpPost]
     [SwaggerOperation(Summary = "Criar um novo exemplo", Description = "Cria um novo exemplo com os dados fornecidos.")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult> Post(CreateExampleCommand example)
@@ -45,6 +49,7 @@ public class ExampleController : BaseApiController
 
     [HttpPut]
     [SwaggerOperation(Summary = "Atualizar um exemplo", Description = "Atualiza um exemplo existente com base nos dados fornecidos.")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -56,6 +61,7 @@ public class ExampleController : BaseApiController
 
     [HttpDelete("{id:int}")]
     [SwaggerOperation(Summary = "Deletar um exemplo pelo ID", Description = "Remove um exemplo com base no ID fornecido.")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status204NoContent)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult> Delete(int id)
--- API/CAEFMR/src/Presentation/CAEFMR.Api/Controllers/v2/ExampleController.cs
+++ API/CAEFMR/src/Presentation/CAEFMR.Api/Controllers/v2/ExampleController.cs
@@ -3,15 +3,18 @@ using CAEFMR.Application.Features.Example.Queries.GetById;
 using CAEFMR.Application.Features.Example.Queries.GetList;
 using CAEFMR.Application.Features.Example.Queries.GetPagedList;
 using CAEFMR.Domain.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace CAEFMR.Api.Controllers.v2;
 
 [ApiVersion("2")]
+[Authorize(Roles = "Administrator")]
 public class ExampleController : BaseApiController
 {
     [HttpGet]
+    [AllowAnonymous]
     [SwaggerOperation(Summary = "Obter todos os exemplos", Description = "Retorna uma lista de todos os exemplos.")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<List<ExampleDto>>> Get()
@@ -22,6 +25,7 @@ public class ExampleController : BaseApiController
 
     [HttpGet("{id:int}")]
     [SwaggerOperation(Summary = "Obter um exemplo pelo ID", Description = "Retorna um exemplo específico com base no ID fornecido.")]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<ActionResult<ExampleDto>> GetById(int id)
@@ -30,6 +34,7 @@ public class ExampleController : BaseApiController
         return Ok(example);
     }
 
+    [AllowAnonymous]
     [HttpGet("paged")]
     [SwaggerOperation(Summary = "Obter lista paginada de exemplos", Description = "Retorna uma lista paginada de exemplos.")]
     [ProducesResponseType(StatusCodes.Status200OK)]
